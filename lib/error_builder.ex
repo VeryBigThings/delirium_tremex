c80ac149ff04459771491bebef80afe433b256be
Stanko K.R <me@stanko.io>
2018-10-31 18:48:25 +0100

    Re-license project under the MIT license

--- lib/error_builder.ex
+++
@@ -1,8 +0,0 @@
-defmodule DeliriumTremex.ErrorBuilder do
-  def unauthorized do
-    %{
-      message: "You have insufficient privileges to access this resource",
-      messages: ["You have insufficient privileges to access this resource"]
-    }
-  end
-end
